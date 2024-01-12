d326c18605c0d8c800b06aa0518719c743d26416
susalmmul <9868ksy@gmail.com>
2024-01-12 17:50:37 +0900

    저장

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,17 +7,24 @@ import { useState } from 'react';
 function App() {
 
   let post = "강남 우동 맛집";
-  let [글제목, 글바꾸기] = useState(["여자 코트 추천", "강남 우동 맛집", "파이썬 독학"]);
+  let [글제목, 글제목변경] = useState(["긴 코트 추천", "강남 우동 맛집", "파이썬 독학"]);
   let [따봉, 따봉변경] = useState(0)
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
+      <button onClick={()=> {
+        let copy = [...글제목];
+        copy.sort()
+        글제목변경(copy);
+      }}>글순서변경</button>
+
       <button onClick={() => {
-        글제목[0] = "바꿨다";
-        글바꾸기(글제목);
+        let copy = [...글제목]; //[...] 깊은 복사 : 주소가 아닌, 프로퍼티 값만 복사하는 것. (독립적)
+        copy[0] = "짧은코트 추천";
+        글제목변경(copy);
       }}>글수정</button>
       <div className='list'>
         <h4>{글제목[0]} <span onClick={function (){따봉변경(따봉 + 1)}}>❤️</span> {따봉} </h4>
@@ -30,6 +37,12 @@ function App() {
       <div className='list'>
         <h4>{글제목[2]} <span onClick={function (){따봉변경(따봉 + 1)}}>❤️</span> {따봉} </h4>
         <p>2월 17일 발행</p>
+      </div>
+
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
       </div>
     </div>
   );
